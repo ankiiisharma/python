91b28ec4adf6acdcd8bdeba9cf78da0684c1ac31
Ankiiisharma <myselfankit.2003@gmail.com>
2023-05-18 21:14:28 +0530

    --

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(thisdict["brand"])
+print(thisdict["electric"])
+print(thisdict["year"])
