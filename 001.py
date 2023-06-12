d581de4600c102a2036807bf6ca30a389f76498a
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-12 21:08:27 +0530

    String

---
+++ 001.py
@@ -0,0 +1,7 @@
+a = "hello,My name is Ankit Sharma"
+
+ans=input("Enter word to search  ")
+if ans not in a:
+    print(ans, " is not present in String")
+else:
+    print(ans, " is present in String")
