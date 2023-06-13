4f87a4d05f3664540953ee7171e3de7f924b5e32
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-13 08:41:59 +0530

    Tower of hanoi 2

--- towerofhanoi.py
+++ towerofhanoi.py
@@ -1,12 +1,10 @@
-def TowerOfHanoi(n,source,destination,intermediate):
+def TowerOfHanoi(n, source, destionation, intermediate):
     if(n==1):
-        print("Move disk 1 from pole ",source, " to " ,destination)
+        print("Move 1 from",source, "to",destionation)
         return
-    
-    TowerOfHanoi(n-1, source,intermediate,destination)
-    print("Move disk ",n, "from pole ", source, " to ", destination)
-    TowerOfHanoi(n-1,intermediate,destination,source)
-
+    TowerOfHanoi(n-1,source,intermediate,destionation)
+    print("Move ",n,"from",source, "to", destionation )
+    TowerOfHanoi(n-1,intermediate,destionation,source)
 
 n=3
 TowerOfHanoi(n,'A','C','B')
