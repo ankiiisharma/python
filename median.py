f5b4cc24dab982980ae97e1591d50b25777a2d8e
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-16 23:58:44 +0530

    Median

---
+++ median.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num3 = num1 + num2
+        ans = float(median(num3))
+        return
+
+        print(ans)
+
+        
+        
+        num3 = []
+        n = int(input(""))
+        for i in range(n)
+        list.append(n)
+
+        num2 = []
+        m = int(input(""))
+        for i in range(m)
+        num2.append(m)
+        findMedianSortedArrays(self, nums1: List[int], nums2: List[int])
